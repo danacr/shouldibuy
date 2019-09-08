9ba4d43374372a663b2797958f7fb401dc61558e
danacr <dan@acristinii.com>
2019-09-08 17:39:35 +0200

    workflow version

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
+![](https://github.com/danacr/shouldibuy/workflows/Node%20CI/badge.svg)
+
 ## Available Scripts
 
 In the project directory, you can run:
--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
-let price = 150
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import logo from "./logo.svg";
+import "./App.css";
+let price = 150;
 
 function App() {
-
   const [eth, setData] = useState({ data: [] });
   useEffect(() => {
     const fetchData = async () => {
       const result = await axios(
-        'https://api.coinbase.com/v2/prices/ETH-USD/buy',
+        "https://api.coinbase.com/v2/prices/ETH-USD/buy"
       );
       setData(result.data);
     };
@@ -25,32 +24,27 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <div>
           <pre>
-
-  {eth.data.base} price is {eth.data.amount} {eth.data.currency}
-
+            {eth.data.base} price is {eth.data.amount} {eth.data.currency}
           </pre>
         </div>
         <a
-    className="App-link"
-    href="https://www.coinbase.com/join/acrist_0"
-    target="_blank"
-    rel="noopener noreferrer">
-
-           {Buy(eth.data.amount)}
-           </a>
-        
+          className="App-link"
+          href="https://www.coinbase.com/join/acrist_0"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {Buy(eth.data.amount)}
+        </a>
       </header>
     </div>
   );
 }
 
 function Buy(eth) {
-  if (eth<price) {
-    return  "Buy"
+  if (eth < price) {
+    return "Buy";
   }
-  return (
-    "Wait a bit"
-  )
+  return "Wait a bit";
 }
 
 export default App;
