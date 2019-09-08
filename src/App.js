2752ffad48cfda8d8fbf79caafa2c0934a0c9449
danacr <dan@acristinii.com>
2019-09-08 17:09:43 +0200

    first version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
+let price = 150
 
 function App() {
+
+  const [eth, setData] = useState({ data: [] });
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios(
+        'https://api.coinbase.com/v2/prices/ETH-USD/buy',
+      );
+      setData(result.data);
+    };
+    fetchData();
+  }, []);
+
+  // console.log()
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <pre>
+
+  {eth.data.base} price is {eth.data.amount} {eth.data.currency}
+
+          </pre>
+        </div>
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    className="App-link"
+    href="https://www.coinbase.com/join/acrist_0"
+    target="_blank"
+    rel="noopener noreferrer">
+
+           {Buy(eth.data.amount)}
+           </a>
+        
       </header>
     </div>
   );
+}
+
+function Buy(eth) {
+  if (eth<price) {
+    return  "Buy"
+  }
+  return (
+    "Wait a bit"
+  )
 }
 
 export default App;
